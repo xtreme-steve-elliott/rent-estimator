c95b37d47fca5c11343a860f358af14cc426d8c2
Soren Yeadon <syeadon@vmware.com>
2022-06-04 19:48:32 -0400

    Sets swagger ui to automatically open in every non prod env

--- rent-estimator.Application/Program.cs
+++ rent-estimator.Application/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
